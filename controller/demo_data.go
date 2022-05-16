44ccb3acd0180b8c7981bcb1a30c3d1244d8552b
HavEWinTao <854540702@qq.com>
2022-05-16 21:55:56 +0800

    初始化

---
+++ controller/demo_data.go
@@ -0,0 +1,47 @@
+package controller
+
+var DemoVideos = []Video{
+	{
+		Id:            1,
+		Author:        DemoUser,
+		PlayUrl:       "https://www.w3schools.com/html/movie.mp4",
+		CoverUrl:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
+		FavoriteCount: 0,
+		CommentCount:  0,
+		IsFavorite:    false,
+	},
+	{
+		Id:            2,
+		Author:        UserFan,
+		PlayUrl:       "http://43.138.10.134:8080/static/2_share_1fde94aa2f4803329cb4be99f15bdace.mp4",
+		CoverUrl:      "https://cdn.pixabay.com/photo/2016/03/27/18/10/bear-1283347_1280.jpg",
+		FavoriteCount: 0,
+		CommentCount:  0,
+		IsFavorite:    false,
+	},
+}
+
+var DemoComments = []Comment{
+	{
+		Id:         1,
+		User:       DemoUser,
+		Content:    "Test Comment",
+		CreateDate: "05-01",
+	},
+}
+
+var DemoUser = User{
+	Id:            1,
+	Name:          "TestUser",
+	FollowCount:   0,
+	FollowerCount: 0,
+	IsFollow:      false,
+}
+
+var UserFan = User{
+	Id:            2,
+	Name:          "樊tastic",
+	FollowCount:   10,
+	FollowerCount: 10,
+	IsFollow:      false,
+}
